cdb6fa42a492e584356d8e7a33e119a7a8e8bd90
Cedric Arnould <sidekickinteractive@carnould.com>
2017-09-15 18:09:48 -0400

    Added Angular 4 + HMR

--- Startup.cs
+++ Startup.cs
@@ -1,40 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Test1
+public class Startup
 {
-    public class Startup
+  private readonly IHostingEnvironment _hostingEnvironment;
+
+  public Startup(IHostingEnvironment env)
+  {
+    _hostingEnvironment = env;
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables();
+    Configuration = builder.Build();
+  }
+
+  public IConfigurationRoot Configuration { get; }
+
+  // This method gets called by the runtime. Use this method to add services to the container.
+  public void ConfigureServices(IServiceCollection services)
+  {
+    // Add framework services.
+    services.AddMvc();
+  }
+
+  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+  {
+    // only enable webpack building in Developement environment
+    if (env.IsDevelopment())
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
+      app.UseDeveloperExceptionPage();
+      app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+      {
+        HotModuleReplacement = true
+      });
     }
+
+    app.UseStaticFiles();
+
+    loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+    loggerFactory.AddDebug();
+
+    app.UseMvc(routes =>
+    {
+      routes.MapRoute(
+          name: "default",
+          template: "api/{controller}/{id?}");
+      // add a special route for our index page
+      routes.MapSpaFallbackRoute(
+          name: "spa-fallback",
+          defaults: new { controller = "Home", action = "index" });
+    });
+  }
 }
