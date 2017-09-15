94e732d7b09f1dab3ec05aecc1d157d20ec9d6c1
Cedric Arnould <sidekickinteractive@carnould.com>
2017-09-15 18:46:17 -0400

    Added Angular Material 2

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdInputModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MdButtonModule,
+		MdCheckboxModule,
+		MdInputModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Test1
 
-This project was created based on this article `https://alexklingenbeck.de/articles/17-05-angular-asp-net-core.html`.
+I use this project as test project. 
 
 ## Technologies:
-
-This project uses: Asp.Net Core 2.0, Angular 4, HMR
+This project uses:
+- Asp.Net Core 2.0, Angular 4, HMR (`https://alexklingenbeck.de/articles/17-05-angular-asp-net-core.html`.)
+- Angular Material 2 (`https://material.angular.io/guide/getting-started`)
