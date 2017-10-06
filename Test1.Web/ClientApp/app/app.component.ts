8e56d823f8961dfd140a3d50d6ad3defeccd845d
Cedric Arnould <sidekickinteractive@carnould.com>
2017-10-06 17:55:24 -0400

    Updated the UI

--- Test1.Web/ClientApp/app/app.component.ts
+++ Test1.Web/ClientApp/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { TdMediaService } from '@covalent/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	constructor(
+		public media: TdMediaService,
+	) { }
+
+	ngAfterViewInit(): void {
+		this.media.broadcast();
+	}
 }
--- Test1.Web/ClientApp/app/app.module.ts
+++ Test1.Web/ClientApp/app/app.module.ts
@@ -16,13 +16,74 @@ import { DashboardModule } from './components/dashboard/dashboard.module';
 import { SamplesModule } from './components/samples/samples.module';
 import { NoConflictStyleCompatibilityMode } from '@angular/material'
 
+import * as _fromAngularMaterial from '@angular/material';
+import * as _fromCovalent from '@covalent/core';
+
 @NgModule({
 	imports: [
 		BrowserModule,
 		FormsModule,
 		HttpModule,
 		BrowserAnimationsModule,
-    NoConflictStyleCompatibilityMode,
+		NoConflictStyleCompatibilityMode,
+
+		//Material
+		_fromAngularMaterial.MatAutocompleteModule,
+		_fromAngularMaterial.MatButtonModule,
+		_fromAngularMaterial.MatButtonToggleModule,
+		_fromAngularMaterial.MatCardModule,
+		_fromAngularMaterial.MatCheckboxModule,
+		_fromAngularMaterial.MatChipsModule,
+		_fromAngularMaterial.MatCommonModule,  
+		_fromAngularMaterial.MatDatepickerModule,
+		_fromAngularMaterial.MatDialogModule,
+		_fromAngularMaterial.MatExpansionModule,
+		_fromAngularMaterial.MatFormFieldModule,
+		_fromAngularMaterial.MatGridListModule,
+		_fromAngularMaterial.MatIconModule,
+		_fromAngularMaterial.MatInputModule,
+		_fromAngularMaterial.MatLineModule,
+		_fromAngularMaterial.MatListModule,
+		_fromAngularMaterial.MatMenuModule,
+		_fromAngularMaterial.MatNativeDateModule,
+		_fromAngularMaterial.MatOptionModule,
+		_fromAngularMaterial.MatPaginatorModule,
+		_fromAngularMaterial.MatProgressBarModule,
+		_fromAngularMaterial.MatProgressSpinnerModule,
+		_fromAngularMaterial.MatPseudoCheckboxModule,
+		_fromAngularMaterial.MatRadioModule,
+		_fromAngularMaterial.MatRippleModule,
+		_fromAngularMaterial.MatSelectModule,
+		_fromAngularMaterial.MatSidenavModule,
+		_fromAngularMaterial.MatSliderModule,
+		_fromAngularMaterial.MatSlideToggleModule,
+		_fromAngularMaterial.MatSnackBarModule,
+		_fromAngularMaterial.MatSortModule,
+		_fromAngularMaterial.MatStepperModule,
+		_fromAngularMaterial.MatTableModule,
+		_fromAngularMaterial.MatTabsModule,
+		_fromAngularMaterial.MatToolbarModule,
+		_fromAngularMaterial.MatTooltipModule,       
+
+    //Covalent
+		_fromCovalent.CovalentChipsModule,
+		_fromCovalent.CovalentCommonModule,
+		_fromCovalent.CovalentDataTableModule,
+		_fromCovalent.CovalentDialogsModule,
+		_fromCovalent.CovalentExpansionPanelModule,
+		_fromCovalent.CovalentFileModule,
+		_fromCovalent.CovalentJsonFormatterModule,
+		_fromCovalent.CovalentLayoutModule,
+		_fromCovalent.CovalentLoadingModule,
+		_fromCovalent.CovalentMediaModule,
+		_fromCovalent.CovalentMenuModule,
+		_fromCovalent.CovalentMessageModule,
+		_fromCovalent.CovalentNotificationsModule,
+		_fromCovalent.CovalentPagingModule,
+		_fromCovalent.CovalentSearchModule,
+		_fromCovalent.CovalentStepsModule,
+		_fromCovalent.CovalentVirtualScrollModule,
+
 
 		//Test1
 		AppRoutingModule,
